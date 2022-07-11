44d267fe6e3dbc18862296b58c1666c61f83321a
leyla filicha <tululeyla7@gmail.com>
2022-07-11 11:57:55 -0700

    a parameter taken pointer points to some value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 void reset_to_98(int *n)
 {                                                                                                                                                                                                                                             
 *n=98;
