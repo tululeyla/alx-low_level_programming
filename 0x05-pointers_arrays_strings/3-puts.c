7d51d1d8dd658b9295a2db4054654e155e5f14bb
leyla filicha <tululeyla7@gmail.com>
2022-07-11 18:45:46 -0700

    function that prints a string, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,25 +3,24 @@
  * @str: checked
  * Return:nothing
  */
-
 void _puts(char *str)
 
 {
 
-	if (str)
+		if (str)
 
-	{
+				{
 
-		while (*str)
+							while (*str)
 
-		{
+										{
 
-			_putchar(*str++);
+														_putchar(*str++);
 
-		}
+																}
 
-	}
+								}
 
-	_putchar('\n');
+			_putchar('\n');
 
 }
