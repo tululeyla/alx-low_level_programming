b09e849a1646183e0115b2a47fbb4ecc7001248e
leyla filicha <tululeyla7@gmail.com>
2022-07-11 18:06:13 -0700

    a ponter points to some value

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,10 +1,22 @@
-void reset_to_98(int *n){
-int a;
-n=&a;
-}
-int main(){
-int n;
-n=98;
-reset_to_98(&n);
-return(0);
+#include <stdio.h>
+
+/**
+ *
+ *  reset_to_98- it reset to 98
+ *
+ *  @n: checked
+ *
+ *  Return: nothing
+ *
+ 
+
+void reset_to_98(int *n)
+	
+{
+	
+		int *ptr;
+	
+		ptr = n;			
+	
+		*ptr = 98;				
 }
