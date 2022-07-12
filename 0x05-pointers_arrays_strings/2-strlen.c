d7654203a2f1676f5e783414765db52cead13347
leyla filicha <tululeyla7@gmail.com>
2022-07-11 18:33:38 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+/**
+ * _strlen- counts string exccept \0
+ * @s: checked
+ * Return: i
+ */
+
+int _strlen(char *s)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; s[i] != '\0'; ++i)
+
+	{
+
+		;
+
+	}
+
+	return (i);
+
+
+
+}
+
+
