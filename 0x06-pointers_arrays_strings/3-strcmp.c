a88e43d2566b16831de15ce43effbdaf4fb5b616
Leyla Filicha <tululeyla7@gmail.com>
2022-07-14 14:18:38 -0700

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,28 @@
 
 int strcmp(char *s1, char *s2)
 {
-	int i;
-	int r;
-
-	i=0;
-
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	int i = 0, diff = 0;
+	while (1)
 	{
-		i++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+	diff = s2[i];
+	break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	R = s1[i] - s2[i];
-	return (R);
+	return (diff);
 }
