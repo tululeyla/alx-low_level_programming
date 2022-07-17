4eb10b9ae9375739e76404232c4dec527bd3c76d
Leyla Filicha <tululeyla7@gmail.com>
2022-07-17 05:06:56 -0700

    ariables if else

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -27,13 +27,14 @@ int main(void)
 						putchar(digit1 + 48);
 						putchar(digit2 + 48);
 						putchar(digit3 + 48);
+						}
 
 						if (digit1 + digit2 + digit3 != 24)
 						{
 							putchar(',');
 							putchar(' ');
 						}
-						}
+						
 				++digit3;
 			}
 			++digit2;
