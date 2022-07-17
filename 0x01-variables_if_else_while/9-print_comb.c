b440bb7123b8c8efa02d8f456c1b5287e7b75e54
Leyla Filicha <tululeyla7@gmail.com>
2022-07-17 03:38:58 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print numbers of base16 in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 48; /*48; decimal rep of 0*/
+
+	while (digit <= 102) /*102; decimal rep of f*/
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+	return (0);
+}
